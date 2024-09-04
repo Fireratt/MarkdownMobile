1fe8f096cc390e8d365bd67559bb6f1c84369d71
fireratt <m18218523011@163.com>
2024-09-04 20:49:53 +0800

    Initialize the project

---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+using Markdig;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public String Read()    // transform the markdown to html
+        {
+            var mdPipeLine = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+
+            return Markdig.Markdown.ToHtml(Canvas.Text , mdPipeLine); 
+        }
+        public void OnChange(object sender, TextChangedEventArgs e)
+        {
+            Canvas.Text = MarkdownEditor.Text;
+            MarkdownView.Html = Read(); 
+        }
+
+        public void onSave()
+        {
+
+        }
+        public void onSaveAs()
+        {
+
+        }
+    }
+    
+
+}
---
+++ markdownCanvas.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Markdig;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Graphics.Text;
+using Microsoft.Maui.Graphics.Text.Renderer;
+namespace MauiApp1
+{
+    public class MarkdownCanvas : GraphicsView
+    {
+        public MarkdownCanvas()
+        {
+            WidthRequest = 300;
+            HeightRequest = 300;
+            HorizontalOptions = Microsoft.Maui.Controls.LayoutOptions.Fill;
+            VerticalOptions = Microsoft.Maui.Controls.LayoutOptions.Fill; 
+            Drawable = new MarkdownDrawable(this);
+        }
+        // bind the property 
+        public static readonly BindableProperty TextProperty =
+        BindableProperty.Create(nameof(Text), typeof(string), typeof(MarkdownCanvas), default(string));
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set {
+                SetValue(TextProperty, value);
+                // trigger the re-draw event 
+                this.Invalidate(); 
+            }
+
+        }
+        public class MarkdownDrawable : IDrawable
+        {
+            private MarkdownCanvas parent;
+            public MarkdownDrawable(MarkdownCanvas outCanvas)
+            {
+                this.parent = outCanvas;
+            }
+            public void Draw(ICanvas canvas, RectF dirtyRect)
+            {
+                //canvas.DrawText(Read(), 0, 0, (float)parent.WidthRequest, (float)parent.HeightRequest);
+            }
+            public String Read()    // Transform markdown to html . 
+            {
+                Console.WriteLine(MarkdownAttributedTextReader.Read(parent.Text).Text);
+                return parent.Text; 
+                //return MarkdownAttributedTextReader.Read(parent.Text);
+            }
+        }
+
+    }
+
+}
